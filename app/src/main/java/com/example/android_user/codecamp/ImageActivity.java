a24cac90bf8504b7890e92f27a31e75189a9f0a6
android-user <android-user@fv.amacclab.edu.ph>
2018-04-07 16:52:44 +0800

    so effin smart

--- app/src/main/java/com/example/android_user/codecamp/ImageActivity.java
+++ app/src/main/java/com/example/android_user/codecamp/ImageActivity.java
@@ -3,12 +3,50 @@ package com.example.android_user.codecamp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import com.example.android_user.codecamp.R;
 
 public class ImageActivity extends AppCompatActivity {
+    private Button btPic1;
+    private Button btPic2;
+    private Button btPic3;
+    private ImageView imgPic;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+        buttonbleppp();
+    }
+
+    private void buttonbleppp()
+    {
+        imgPic = findViewById(R.id.imgPic);
+        btPic1 = findViewById(R.id.btPic1);
+        btPic1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgPic.setImageDrawable(getDrawable(R.drawable.heyya));
+            }
+        });
+        btPic2 = findViewById(R.id.btPic2);
+        btPic2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgPic.setImageDrawable(getDrawable(R.drawable.hmmmmm));
+            }
+        });
+        btPic3 = findViewById(R.id.btPic3);
+        btPic3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgPic.setImageDrawable(getDrawable(R.drawable.reeeeeeeee));
+            }
+        });
+
+
     }
 }
